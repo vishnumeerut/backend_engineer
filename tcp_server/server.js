3d647cd4ebe8049705f72bb6ab1c91d4fee123a8
Vishnu Kumar <vermaji1891@gmail.com>
2025-09-26 16:36:41 +0530

    added chat app using tcp

--- tcp_server/server.js
+++ tcp_server/server.js
@@ -1,13 +1,34 @@
 import net from "node:net";
 
-
+const clients = [];
 const server = net.createServer((socket) => {
     console.log("Client Connected..")
-    socket.on("data", (chunk) => {
-        console.log("Data Received by server:-", chunk.toString());
 
-        socket.write(`Received by client:-> ${chunk}`);
-        socket.end()
+    clients.push(socket);
+    console.log(`Total clients are:- ${clients.length} \n`)
+
+    socket.write("Welcome to the broadcast Chat: \n")
+    socket.write("Type your message and press enter: \n")
+
+
+    socket.on("data", (chunk) => {
+        console.log("Data Received by server:-", chunk.toString().trim());
+
+        clients.forEach((client) => {
+
+            client.write(`Received by client:-> ${chunk} \n`);
+        })
+
+
+        socket.on("end", () => {
+            const index = clients.indexOf(socket)
+            if(index !== -1) {
+            clients.splice(index, 1)
+        }
+        })
+        console.log("One Client disconnected")
+        console.log(`Total clients are:- ${clients.length} \n`)
+
     })
 
     socket.on("error", (err) => {
